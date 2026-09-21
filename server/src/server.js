055744e7e9936e059e8588215006c65f468c833f
DOVYDAS <dovydassirmulis@gmail.com>
2026-09-21 21:54:37 +0300

    kuriamas expres serveris

---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("api/health", (req,res) => {
+    res.json({message: "Serveris veikia"});
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Serveris paleistas ant porto ${PORT}`);
+
+});
