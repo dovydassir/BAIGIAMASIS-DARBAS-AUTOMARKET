c76cd5060e278774fea88d90b6a13273f33832e7
DOVYDAS <dovydassirmulis@gmail.com>
2026-09-23 19:30:50 +0300

    žinutė: pridetas .env nustatymas

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.get("/api/health", (req, res) => {
     res.json({message: "Serveris veikia"});
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Serveris paleistas ant porto ${PORT}`);
