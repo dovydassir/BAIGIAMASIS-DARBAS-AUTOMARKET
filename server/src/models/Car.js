98dee3cc4713957ba100d7db777495232ceebc7b
DOVYDAS <dovydassirmulis@gmail.com>
2026-09-23 21:04:34 +0300

    žinutė: pridetas automobilio modelis

---
+++ server/src/models/Car.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const carSchema = new mongoose.Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    fuel: {
+      type: String,
+    },
+    mileage: {
+      type: Number,
+    },
+    imageUrl: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Car", carSchema);
